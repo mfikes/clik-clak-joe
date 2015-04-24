08de980d6a534c250abb5cd7a60a5f9b45c54c05
Mike Fikes <mike@fikesfarm.com>
2015-04-24 19:35:43 -0400

    Update to latest ClojureScript

--- ClojureScript/project.clj
+++ ClojureScript/project.clj
@@ -1,8 +1,8 @@
 (defproject clik-clak-joe "0.1.0-SNAPSHOT"
   :description "React Native tic-tac-toe in ClojureScript"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3153"]
-                 [org.omcljs/om "0.8.8"]
+  :dependencies [[org.clojure/clojure "1.7.0-beta1"]
+                 [org.clojure/clojurescript "0.0-3211"]
+                 [org.omcljs/om "0.9.0-SNAPSHOT"]
                  [org.omcljs/ambly "0.1.0-SNAPSHOT"]]
   :plugins [[lein-cljsbuild "1.0.5"]]
   :source-paths ["src"]
