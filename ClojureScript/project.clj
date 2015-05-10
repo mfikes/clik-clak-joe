8e08e9e345efad20c18458d77cdb4e726ddd64a9
Mike Fikes <mike@fikesfarm.com>
2015-05-10 11:41:32 -0400

    Update to released version of Ambly

--- ClojureScript/project.clj
+++ ClojureScript/project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.7.0-beta1"]
                  [org.clojure/clojurescript "0.0-3211"]
                  [org.omcljs/om "0.8.8"]
-                 [org.omcljs/ambly "0.1.0-SNAPSHOT"]]
+                 [org.omcljs/ambly "0.1.0"]]
   :plugins [[lein-cljsbuild "1.0.5"]]
   :source-paths ["src"]
   :clean-targets ["js" "out"]
