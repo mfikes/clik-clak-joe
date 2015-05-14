6026e56067b9b5e8027ede6db99165c92b7ab9a4
Mike Fikes <mike@fikesfarm.com>
2015-05-14 10:15:32 -0400

    Add code that excercises React Native

--- ClojureScript/src/clik_clak_joe/core.cljs
+++ ClojureScript/src/clik_clak_joe/core.cljs
@@ -1,1 +1,22 @@
-(ns clik-clak-joe.core)
+(ns clik-clak-joe.core
+  (:require [clik-clak-joe.pre-om]
+            [om.core :as om]
+            [om.dom :as dom]))
+
+;; Reset js/React back as the form above loads in an different React
+(set! js/React (js/require "react-native/Libraries/react-native/react-native"))
+
+(defn widget [data owner]
+  (reify
+    om/IRender
+    (render [this]
+      (js/React.createElement js/React.View 
+          (clj->js {:style {:flexDirection "row", 
+                            :margin 40 :backgroundColor "#EEFFEE" 
+                            :justifyContent "center"}})
+        (js/React.createElement js/React.Text 
+            (clj->js {:style {:flex 1, 
+                              :justifyContent "center"}}) 
+            (:text data))))))
+
+(om/root widget {:text "Hello from ClojureScript!"} {:target 1})
---
+++ ClojureScript/src/clik_clak_joe/pre_om.cljs
@@ -0,0 +1,4 @@
+(ns clik-clak-joe.pre-om)
+
+;; Need to set js/React first so that Om can load
+(set! js/React (js/require "react-native/Libraries/react-native/react-native"))
